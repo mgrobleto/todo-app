656974905ccf370b4786b9e5ac0b5c9c2ca605a4
Gabriela <magabriela0715@gmai.com>
2023-08-27 21:26:01 -0600

    added react native navigation and task checkbox and text animation

--- src/components/animated-checkbox.tsx
+++ src/components/animated-checkbox.tsx
@@ -1,2 +1,15 @@
 import React, { useEffect, memo} from "react";
-import Animated, { Easing, useSharedValue, useAnimatedProps, withTiming, interpolateColor} from "react-native-reanimated";
+import Animated, { Easing, useSharedValue, useAnimatedProps, withTiming, interpolateColor} from "react-native-reanimated"
+import Svg, { Path, Defs, ClipPath, G } from 'react-native-svg'
+
+const MARGIN = 10
+const vWidth = 64 + MARGIN
+const vHeight = 64 + MARGIN
+
+const AnimatedCheckBox = () => {
+    return (
+        <Svg viewBox={[MARGIN, -MARGIN, vWidth + MARGIN, vHeight + MARGIN].join('')}>
+           
+        </Svg>
+    )
+}
